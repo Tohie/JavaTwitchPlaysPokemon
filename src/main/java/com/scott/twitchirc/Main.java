c8e85db3a135343a5a20a1534c9007746ecf060a
Scott Jenkins <scottdjwales@gmail.com>
2014-02-28 09:14:29 +0000

    Added the ability to press keys in a window of choice

--- src/main/java/com/scott/twitchirc/Main.java
+++ src/main/java/com/scott/twitchirc/Main.java
@@ -1,21 +1,41 @@
 package com.scott.twitchirc;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
+        Map<String, String> commands = new HashMap<String, String>();
+        commands.put("start", "x");
+        commands.put("a", "a");
+        commands.put("b", "b");
+        commands.put("left", "l");
+        commands.put("right", "r");
+        commands.put("up", "u");
+        commands.put("down", "d");
+
         try {
-            TwitchIRC irc = new TwitchIRC("Your nick", "your token");
+            TwitchIRC irc = new TwitchIRC("Tohie", "vgld8t1i4k1rcuxpqicl10usskt3gq");
             irc.connectToChannel("#twitchplayspokemon");
             String message;
             while (true) {
-                message = irc.readMessage();
+                message = irc.readMessage().toLowerCase();
+                System.out.println(message);
                 if (message != null) {
-                    System.out.println(message);
+                    if (commands.containsKey(message)) {
+                        execute(args[0], commands.get(message));
+                    }
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void execute(String window, String button) throws IOException {
+        String command =
+                "xdotool search --desktop 0 " + window + " windowactivate --sync key --clearmodifiers " + button;
+        Runtime.getRuntime().exec(command);
     }
 }
